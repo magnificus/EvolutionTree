0f89536377403ac23c60e7f3e9b50b4ffa260c7f
Tobias Elinder <tobiaselinder@gmail.com>
2016-03-31 22:56:11 +0200

    initial
    Former-commit-id: 50dc7475251a2035c50b230d52197094707eb00c

---
+++ Source/TreeEvolution/Branch.cpp
@@ -0,0 +1,225 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TreeEvolution.h"
+#include "Branch.h"
+
+
+// Sets default values
+ABranch::ABranch()
+{
+
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//static ConstructorHelpers::FObjectFinder<UClass> BranchFinder(TEXT("Class'/Game/BranchBP.BranchBP_C'"));
+	//if (BranchFinder.Object != NULL)
+	//	Branch_BP = BranchFinder.Object;
+
+	//static ConstructorHelpers::FObjectFinder<UClass> LeafFinder(TEXT("Class'/Game/LeafBP.LeafBP_C'"));
+	//if (LeafFinder.Object != NULL)
+	//	Leaf_BP = LeafFinder.Object;
+
+
+	random.GenerateNewSeed();
+
+
+}
+
+// Called when the game starts or when spawned
+void ABranch::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ABranch::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABranch::mutate()
+{
+	// creation branch
+	if (branches.Num() < 1 && random.FRand() < branchMutationChance) {
+		spawnRandomBranch();
+	}
+
+
+	// destruction branch
+
+	if (random.FRand() < branchMutationChance && branches.Num() > 0) {
+		int32 index = random.GetUnsignedInt() % branches.Num();
+		ABranch* b = branches[index];
+		b->annihilate();
+		branches.RemoveAt(index);
+	}
+
+
+	// creation leaf
+
+	if (random.FRand() < leafMutationChance) {
+		spawnRandomLeaf();
+	}
+
+	// destruction leaf
+
+	if (random.FRand() < leafMutationChance && leafs.Num() > 0) {
+		int32 index = random.GetUnsignedInt() % leafs.Num();
+		ALeaf* l = leafs[index];
+		l->Destroy();
+		leafs.RemoveAt(index);
+	}
+
+
+	// propagation
+
+	for (ABranch* a : branches) {
+		a->mutate();
+	}
+
+}
+
+void ABranch::spawnRandomLeaf()
+{
+
+	FTransform trans = getRandomPositionOnBranch();
+	ALeaf* spawnedLeaf = (ALeaf*)GetWorld()->SpawnActor(Leaf_BP);
+
+
+
+	spawnedLeaf->SetActorLocation(trans.GetLocation());
+	spawnedLeaf->SetActorRotation(trans.Rotator());
+
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, "Spawned Leaf");
+	leafs.Add(spawnedLeaf);
+
+
+}
+
+void ABranch::spawnRandomBranch() {
+
+	ABranch* spawnedBranch = (ABranch*) GetWorld()->SpawnActor(Branch_BP);
+
+	FTransform trans = getRandomPositionOnBranch();
+
+	spawnedBranch->SetActorLocation(trans.GetLocation());
+	spawnedBranch->SetActorRotation(trans.Rotator());
+
+	branches.Add(spawnedBranch);
+
+}
+void ABranch::annihilate() {
+	for (ABranch* b : branches) {
+		b->annihilate();
+	}
+	for (ALeaf* l : leafs) {
+		l->Destroy();
+	}
+	Destroy();
+}
+
+FTransform ABranch::getRandomPositionOnBranch() {
+
+	// beginning and end of branch...
+	TArray<UPrimitiveComponent*> comps;
+	this->GetComponents(comps);
+	FVector begin;
+	FVector end;
+	for (auto Itr(comps.CreateIterator()); Itr; ++Itr)
+	{
+		if ((*Itr)->GetName() == "start") {
+			begin = (*Itr)->GetComponentLocation();
+		}
+		else if ((*Itr)->GetName() == "end")
+		{
+			end = (*Itr)->GetComponentLocation();
+
+		}
+	}
+	FVector pos = begin + (random.FRand() * (end - begin));
+
+	FVector beamStart = pos;
+	float distX = (random.FRand() - .5) * 200;
+	float distY = (random.FRand() - .5) * 200;
+	float distZ = (random.FRand() - .5) * 200;
+
+	beamStart.X += distX;
+	beamStart.Y += distY;
+	beamStart.Z += distZ;
+
+	FCollisionQueryParams RV_TraceParams = FCollisionQueryParams(FName(TEXT("RV_Trace")), true);
+	RV_TraceParams.bTraceComplex = true;
+	RV_TraceParams.bTraceAsyncScene = true;
+	RV_TraceParams.bReturnPhysicalMaterial = false;
+
+
+	//Re-initialize hit info
+	FHitResult RV_Hit(ForceInit);
+
+	//DrawDebugLine(
+	//	GetWorld(),
+	//	beamStart,
+	//	pos,
+	//	FColor(0, 255, 0),
+	//	true, -1, 0,
+	//	1
+	//	);
+	GetWorld()->LineTraceSingleByChannel(
+		RV_Hit,        //result
+		beamStart,    //start
+		pos, //end
+		ECC_Camera, //collision channel
+		RV_TraceParams
+		);
+
+	FVector Direction = pos - beamStart;
+	FRotator Rot = FRotationMatrix::MakeFromX(Direction).Rotator();
+	FTransform t;
+	t.SetLocation(RV_Hit.ImpactPoint);
+	t.SetRotation(FQuat(Rot));
+	return t;
+
+}
+
+float ABranch::calculateCost() {
+	float totalCost(0);
+	for (ABranch* b : branches) {
+		totalCost += b->calculateCost();
+	}
+	for (ALeaf* l : leafs) {
+		totalCost += l->cost;
+	}
+	totalCost += cost;
+
+	return totalCost;
+}
+
+void ABranch::addBranch(ABranch * b)
+{
+	branches.Add(b);
+}
+
+void ABranch::addLeaf(ALeaf * l)
+{
+	leafs.Add(l);
+}
+
+//ABranch* ABranch::duplicate(FVector originalLocation, FVector newLocation) {
+
+	//ABranch* spawnedBranch; /*= (ABranch*) spawner.spawnBranch(GetWorld());*/
+	//FVector diff = GetActorLocation() - originalLocation;
+	//spawnedBranch->SetActorLocation(newLocation - diff);
+	//for (ABranch* b : branches) {
+	//	spawnedBranch->addBranch(b->duplicate(originalLocation, newLocation));
+	//}
+	//for (ALeaf* l : leafs) {
+	//	spawnedBranch->addLeaf(l->duplicate(originalLocation, newLocation));
+	//}
+
+	//return spawnedBranch;
+
+//	return this;
+//}
---
+++ Source/TreeEvolution/Tree.cpp
@@ -0,0 +1,206 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TreeEvolution.h"
+#include "Tree.h"
+#include "Leaf.h"
+
+
+// Sets default values
+ATree::ATree()
+{
+	/*static ConstructorHelpers::FObjectFinder<UClass> TreeFinder(TEXT("Class'/Game/TreeBP.TreeBP_C'"));
+	if (TreeFinder.Object != NULL)
+		Tree_BP = TreeFinder.Object;
+
+	static ConstructorHelpers::FObjectFinder<UClass> BranchFinder(TEXT("Class'/Game/BranchBP.BranchBP_C'"));
+	if (BranchFinder.Object != NULL)
+		Branch_BP = BranchFinder.Object;*/
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	random.GenerateNewSeed();
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ATree::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ATree::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+float ATree::calculateHits() {
+	FVector startLocation = GetActorLocation();
+	startLocation.Z += zDist;
+
+	int hits = 0;
+	FCollisionQueryParams RV_TraceParams = FCollisionQueryParams(FName(TEXT("RV_Trace")), true, this);
+	RV_TraceParams.bTraceComplex = true;
+	RV_TraceParams.bTraceAsyncScene = true;
+	RV_TraceParams.bReturnPhysicalMaterial = false;
+
+
+	//Re-initialize hit info
+	FHitResult RV_Hit(ForceInit);
+
+	FVector currStart;
+	FVector currEnd;
+	float relation = maxSpread / numberRays;
+	for (int i = -numberRays / 2; i < numberRays / 2; ++i) {
+		for (int j = -numberRays / 2; j < numberRays / 2; ++j) {
+			currStart = startLocation;
+			currStart.X += relation * i;
+			currStart.Y += relation * j;
+
+			currEnd = currStart;
+			currEnd.Z -= zDist;
+
+			//call GetWorld() from within an actor extending class
+			GetWorld()->LineTraceSingleByChannel(
+				RV_Hit,        //result
+				currStart,    //start
+				currEnd, //end
+				ECC_Pawn, //collision channel
+				RV_TraceParams
+				);
+			if (debugLine) {
+				DrawDebugLine(
+					GetWorld(),
+					currStart,
+					currEnd,
+					FColor(255, 0, 0),
+					true, -1, 0,
+					1
+					);
+			}
+
+			if (RV_Hit.bBlockingHit) {
+				ALeaf* leaf = Cast<ALeaf>(RV_Hit.GetActor());
+				if (leaf) {
+					hits++;
+				}
+
+			}
+		}
+	}
+
+	return hits / (numberRays*numberRays);
+
+}
+
+float ATree::calculateCost()
+{
+	float cost(0);
+	for (ABranch* b : branches) {
+		cost += b->calculateCost();
+	}
+	return cost;
+}
+
+void ATree::mutate() {
+	float f = random.FRand();
+
+	if (f < mutationChance) {
+		spawnRandomBranch();
+	}
+
+	f = random.FRand();
+
+	if (f < mutationChance && branches.Num() > 0) {
+		int32 index = random.GetUnsignedInt() % branches.Num();
+		ABranch* b = branches[index];
+		b->annihilate();
+		branches.RemoveAt(index);
+	}
+
+
+	for (ABranch* b : branches) {
+		b->mutate();
+	}
+
+}
+
+void ATree::spawnRandomBranch() {
+	FVector target = GetActorLocation();
+	target.Z += random.FRand() * 300;
+
+	FVector beamOrigin = target;
+
+	float distX = (random.FRand() - .5) * 500;
+	float distY = (random.FRand() - .5) * 500;
+	beamOrigin.X += distX;
+	beamOrigin.Y += distY;
+
+	FCollisionQueryParams RV_TraceParams = FCollisionQueryParams(FName(TEXT("RV_Trace")), true);
+	RV_TraceParams.bTraceComplex = true;
+	RV_TraceParams.bTraceAsyncScene = true;
+	RV_TraceParams.bReturnPhysicalMaterial = false;
+
+
+	//Re-initialize hit info
+	FHitResult RV_Hit(ForceInit);
+
+	if (debugLine) {
+		DrawDebugLine(
+			GetWorld(),
+			beamOrigin,
+			target,
+			FColor(255, 0, 0),
+			true, -1, 0,
+			1
+			);
+	}
+	
+	GetWorld()->LineTraceSingleByChannel(
+		RV_Hit,        //result
+		beamOrigin,    //start
+		target, //end
+		ECC_Camera, //collision channel
+		RV_TraceParams
+		);
+
+	if (RV_Hit.bBlockingHit) {
+		ABranch* spawnedBranch = (ABranch*)GetWorld()->SpawnActor(Branch_BP);
+
+		FVector Direction = target - beamOrigin;
+		FRotator Rot = FRotationMatrix::MakeFromX(Direction).Rotator();
+
+		spawnedBranch->SetActorLocation(RV_Hit.ImpactPoint);
+		spawnedBranch->SetActorRotation(Rot);
+
+
+
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Spawned Branch");
+		branches.Add(spawnedBranch);
+
+
+	}
+
+}
+
+//ATree* ATree::duplicate(FVector location) {
+	//ATree* spawnedTree = (ATree*)GetWorld()->SpawnActor(Tree_BP);
+
+	//spawnedTree->SetActorLocation(location);
+
+	//for (ABranch* b : branches) {
+	//	spawnedTree->addBranch(b->duplicate(GetActorLocation(), location));
+	//}
+
+	//return spawnedTree;
+	//return this;
+
+//}
+
+void ATree::addBranch(ABranch* b) {
+	branches.Add(b);
+}
